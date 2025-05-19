97683cdefe784166fb1c27f2739c872f05894fa5
milton munoz <livemilton@hotmail.com>
2025-05-18 21:19:51 -0500

    Spring boot rest api - return java List as JSON

--- src/main/java/net/javaguideslivemilton/springboot_rest_api/controller/StudentController.java
+++ src/main/java/net/javaguideslivemilton/springboot_rest_api/controller/StudentController.java
@@ -4,6 +4,9 @@ import net.javaguideslivemilton.springboot_rest_api.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -17,4 +20,17 @@ public class StudentController {
         );
         return student;
     }
+
+    //http://localhost:8080/students
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1,"milton","Munoz"));
+        students.add(new Student(2,"jhon","Munoz"));
+        students.add(new Student(3,"Hilda","Munoz"));
+        students.add(new Student(4,"Ferney","Lozano"));
+
+        return students;
+    }
+
 }
