62372342478f223a50968f71b3cb7ee6bde49aa7
milton munoz <livemilton@hotmail.com>
2025-05-18 21:07:14 -0500

    Spring boot rest api - return java bean as JSON

---
+++ src/main/java/net/javaguideslivemilton/springboot_rest_api/bean/Student.java
@@ -0,0 +1,15 @@
+package net.javaguideslivemilton.springboot_rest_api.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Student {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+}
